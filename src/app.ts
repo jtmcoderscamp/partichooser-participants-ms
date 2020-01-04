c7382cfccedace11ed403187082d2381da7f591c
jtmaluszynski <j.t.maluszynski@gmail.com>
2020-01-04 13:21:17 +0100

    enabled all cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
@@ -9,6 +10,9 @@ import ParticipantService from "./participantsModule/core/ParticipantService";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+//enable cors FOR EVERYTHING
+app.use(cors());
 
 app.use(bodyParser.json());
 
